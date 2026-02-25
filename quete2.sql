e31b8dda5ae9a74a6e50df9fc669a38db8c49cbc
lucas <133487019+Yakolacoco@users.noreply.github.com>
2026-02-25 17:02:33 +0100

    modfication des commentaires
    Updated comments for SQL queries to improve clarity.

--- quete2.sql
+++ quete2.sql
@@ -3,14 +3,14 @@
 -- Notions : ORDER BY / filtres multiples
 -- =====================================================
 
--- 1️⃣ Véhicules triés par autonomie décroissante
+--  Véhicules triés par autonomie décroissante
 SELECT *
 FROM vehicule
 ORDER BY autonomie_km DESC;
 
 --------------------------------------------------------
 
--- 2️⃣ Véhicules disponibles triés par ville puis marque
+--  Véhicules disponibles triés par ville puis marque
 SELECT *
 FROM vehicule
 WHERE etat = 'disponible'
@@ -18,7 +18,7 @@ ORDER BY ville ASC, marque ASC;
 
 --------------------------------------------------------
 
--- 3️⃣ Liste des clients par ordre alphabétique
+--  Liste des clients par ordre alphabétique
 SELECT *
 FROM client
 ORDER BY nom ASC, prenom ASC;
